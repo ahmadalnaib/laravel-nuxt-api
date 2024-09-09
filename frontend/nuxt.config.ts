cfe753c5dc45f53cf17d09d7677e4468b2e69f4d
ahmadalanib <alnaib888@gmail.com>
2024-09-09 19:09:32 +0200

    install larael sanctum package

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,8 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
 
-
   devServer:{
     host: 'http://127.0.0.1:8000/'
+  },
+
+  modules: ['nuxt-auth-sanctum'],
+
+  sanctum:{
+    baseUrl: 'http://localhost:8000'
   }
 })
