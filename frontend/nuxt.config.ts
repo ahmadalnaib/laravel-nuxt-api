38c69e3c3842d0b236d5634be814c58e1c05721d
ahmadalanib <alnaib888@gmail.com>
2024-09-09 18:46:45 +0200

    add the domain in the config file for nuxt

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+
+
+  devServer:{
+    host: 'http://127.0.0.1:8000/'
+  }
 })
