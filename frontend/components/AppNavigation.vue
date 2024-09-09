ed229ef24e8138f4ac7529763bd44a1f1fc88ade
ahmadalanib <alnaib888@gmail.com>
2024-09-09 21:12:57 +0200

    fetching the authen user

--- frontend/components/AppNavigation.vue
+++ frontend/components/AppNavigation.vue
@@ -1,16 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { isAuthenticated,user } = useSanctumAuth();
+</script>
 
 <template>
   <div class="p-6 flex items-center justify-between">
     <div class="flex items-center space-x-6">
-     <nuxt-link to="/">Home</nuxt-link>
-    <nuxt-link to="/dashboard">Dashboard</nuxt-link>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/dashboard">Dashboard</nuxt-link>
     </div>
-    <div class="flex items-center space-x-6">
-
-      <nuxt-link to="/">Account</nuxt-link>
-    
-
+    <div class="flex items-center space-x-6" v-if="!isAuthenticated">
+      <nuxt-link to="/auth/login">Login</nuxt-link>
+    </div>
+    <div class="flex items-center space-x-6" v-if="isAuthenticated">
+      <div class="font-semibold">
+                {{ user.name }}
+      </div>
     </div>
   </div>
 </template>
